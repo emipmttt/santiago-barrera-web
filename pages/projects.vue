9401e3d0c9ac584df567ac06c77592fdda865a05
Christopher <christophergerardy778@gmail.com>
2021-04-09 22:02:18 -0500

    API DE PROJECTOS COMPLETA

---
+++ pages/projects.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+
+    <v-container class="mt-8">
+      <v-row>
+        <v-col class="col-md-12">
+          <h4 class="Hatton-Light text-center">
+            A CARFUL SELECTION OF MY WORK.
+          </h4>
+        </v-col>
+      </v-row>
+    </v-container>
+
+
+    <PortfolioItem />
+    <PortfolioItem />
+    <PortfolioItem />
+    <PortfolioItem />
+    <PortfolioItem />
+  </div>
+</template>
+
+<script>
+import PortfolioItem from "../components/PortfolioItem";
+
+export default {
+  name: "projects",
+
+  data: () => ({
+    projects: []
+  }),
+
+  async mounted() {
+    await this.getAllProjects();
+  },
+
+  components: {
+    PortfolioItem
+  },
+
+  methods: {
+    async getAllProjects() {
+      const response = await fetch('/api/projects');
+      const data = await response.json();
+      console.log(data);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- server/interfaces/routes/index.js
+++ server/interfaces/routes/index.js
@@ -1,8 +1,10 @@
 const users = require("./users.js");
+const projects = require("./projects");
 
 const express = require("express");
 const app = express();
 
 app.use('/user', users);
+app.use('/projects', projects);
 
 module.exports = app;
