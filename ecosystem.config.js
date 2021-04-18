2af027e9328fe33b26039fd67c91981d3f88727a
Emiliano Pamont <emipmttt@gmail.com>
2021-04-18 17:12:43 -0500

    .

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,10 @@
-module.exports = {
-    apps: [
-      {
-        name: 'Santiago Barrera',
-        exec_mode: 'cluster',
-        instances: 'max', // Or a number of instances
-        script: './node_modules/nuxt/bin/nuxt.js',
-        args: 'start'
-      }
-    ]
+module.exports = [
+  {
+    name: "Santiago Barrera",
+    exec_mode: "cluster",
+    instances: "max", // Or a number of instances
+    script: "./node_modules/nuxt/bin/nuxt.js",
+    args: "start",
+    log_file: "records.log"
   }
+];
