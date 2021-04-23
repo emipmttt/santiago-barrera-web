f295504106590f9d7ede2cabb5ad7c0fbef9f682
Carlos Damian Chavez Gonalez <carlos_dami12@live.com>
2021-04-23 13:16:58 -0500

    Maquetado blog (Lista de entradas)

---
+++ pages/journal.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div  class="navigateBar__title Hatton">
+      <p>Journal</p>
+    </div>
+    <div class="article">
+      <item-article :article="article"/>
+      <item-article :article="article"/>
+      <item-article :article="article"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import ItemArticle from "../components/articleJournal/itemArticle";
+export default {
+  name: "journal",
+  components: {ItemArticle},
+  data(){
+    return{
+      article: {
+        title: 'Typography as art and chaos',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo iste libero nulla! Culpa doloribus, eum incidunt laborum maxime totam vero. Aperiam, aut dolorum earum illum modi omnis possimus quia reprehenderit.'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.navigateBar__title{
+  display: flex;
+  justify-content: center;
+  font-size: 2rem;
+  margin-top: 2rem;
+}
+
+.article{
+  margin-bottom: 10rem;
+}
+
+</style>
