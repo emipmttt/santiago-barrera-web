8033b70640f19aa0eb230ee5cec0b1d45b018db0
Christopher <christophergerardy778@gmail.com>
2021-05-07 16:09:39 -0500

    ARTICLE CRUD - FRONT QUILL

--- components/admin_projects/List.vue
+++ components/admin_projects/List.vue
@@ -1,15 +1,16 @@
 <template>
-  <v-simple-table>
-    <template v-slot:default>
-      <thead>
+  <div>
+    <v-simple-table>
+      <template v-slot:default>
+        <thead>
         <tr>
           <th class="text-left"></th>
           <th class="text-left">Title</th>
           <th class="text-left">Date</th>
           <th class="text-left"></th>
         </tr>
-      </thead>
-      <tbody>
+        </thead>
+        <tbody>
         <tr v-for="project in projects" :key="project._id">
           <td>
             <img class="preview" :src="findFirstImage(project.content)" />
@@ -20,24 +21,42 @@
             <v-btn :disabled="loading" @click="rebuildScraping(project._id)">
               <v-icon color="#FF8D3B">mdi-reload</v-icon>
             </v-btn>
+
+            <v-btn :disabled="loading" @click="openEditModal(project)">
+              <v-icon color="#FF8D3B">
+                mdi-pencil
+              </v-icon>
+            </v-btn>
+
             <v-btn :disabled="loading" @click="deleteProject(project._id)">
               <v-icon color="#FF8D3B">mdi-delete</v-icon>
             </v-btn>
           </td>
         </tr>
-      </tbody>
-    </template>
-  </v-simple-table>
+        </tbody>
+      </template>
+    </v-simple-table>
+
+    <EditProjectModal
+      :active="active"
+      :project="projectSelected"
+      @closeModal="closeModal"
+    />
+  </div>
 </template>
 
 <script>
 import axios from "axios";
 import getProjects from "../../middleware/getProjects";
+import EditProjectModal from "./EditProjectModal";
 export default {
+  components: {EditProjectModal},
   data() {
     return {
       projects: [],
       loading: false,
+      active: false,
+      projectSelected: {}
     };
   },
   methods: {
@@ -45,6 +64,16 @@ export default {
       const projects = await getProjects();
       this.projects = [];
       this.projects = projects;
+    },
+
+    openEditModal(project) {
+      this.active = true;
+      this.projectSelected = {...project};
+    },
+
+    closeModal() {
+      this.active = false;
+      this.getProjects_();
     },
 
     findFirstImage(content) {
