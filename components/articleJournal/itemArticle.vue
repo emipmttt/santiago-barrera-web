631d253e6b7fbf69d56b10420515a1c78696f93e
Emiliano Pamont <emipmttt@gmail.com>
2021-05-17 14:20:35 -0500

    showing in the article the correct tags

--- components/articleJournal/itemArticle.vue
+++ components/articleJournal/itemArticle.vue
@@ -1,15 +1,13 @@
 <template>
   <div class="article--container">
-    <nuxt-link
-      :to="`/post/${article._id}`"
-      class="article--containerLink"
-    >
+    <nuxt-link :to="`/post/${article._id}`" class="article--containerLink">
       <div class="article__title NeueMontreal-Bold">
         {{ article.title }}
       </div>
-      <div class="article__content NueMontreal">
-        {{article.body.substr(42,100)}}
-      </div>
+      <div
+        v-html="article.body.substring(0, 100)"
+        class="article__content NueMontreal"
+      ></div>
     </nuxt-link>
   </div>
 </template>
@@ -17,33 +15,31 @@
 <script>
 export default {
   name: "itemArticle",
-  props:{
-    article:{}
-  }
-}
+  props: {
+    article: {},
+  },
+};
 </script>
 
 <style scoped>
-
-.article--containerLink{
+.article--containerLink {
   text-decoration-line: none;
-  color: #1F2020;
+  color: #1f2020;
 }
 
-.article--container{
+.article--container {
   width: 30%;
   margin-left: 5%;
   margin-top: 3rem;
 }
 
-.article__title{
+.article__title {
   font-size: 3.4rem;
   line-height: 3.6rem;
   margin-bottom: 1rem;
 }
 
-.article__content{
+.article__content {
   line-height: 1.3rem;
 }
-
 </style>
